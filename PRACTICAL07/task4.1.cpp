d1edd68d9f5166c8c8df5580718750d7955c3c32
Chandni-Mishra <91174476+Chandni-Mishra@users.noreply.github.com>
2021-10-19 13:34:53 +0530

    Create task4.1.cpp

---
+++ PRACTICAL07/task4.1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[3][3];
+    cout << "Enter the elements of the array" << endl;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+        cin >> arr[i][j];
+        }
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            arr[i][j] = i+1;
+        }
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
