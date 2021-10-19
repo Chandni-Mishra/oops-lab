0830b7de87864d7bf0ec588f4a9de197b284814a
Chandni-Mishra <91174476+Chandni-Mishra@users.noreply.github.com>
2021-10-19 12:04:04 +0530

    Create task3.3.cpp

---
+++ PRACTICAL07/task3.3.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int arr[3][3];
+  cout << "Enter the elements of the array" << endl;
+  for(int i=0;i<3;i++)
+    {
+      for(int j=0;j<3;j++)
+        {
+          cin >> arr[i][j];
+         }
+         }
+   cout << "Reversed array is:" << endl;
+     for(int i=0;i<3;i++)
+       {
+         for(int j=0;j<3;j++)
+         {
+         arr[i][j] = 10-arr[i][j];
+         cout << arr[i][j] << " ";
+       }
+       cout << "\n";
+       }
+  return 0;
+  }
+  
