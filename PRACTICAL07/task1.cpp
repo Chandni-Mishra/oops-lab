fd1da09f3fa664b372942a9f250a60fe6007f30a
Chandni-Mishra <91174476+Chandni-Mishra@users.noreply.github.com>
2021-10-19 11:20:20 +0530

    Create task1.cpp

---
+++ PRACTICAL07/task1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int a;
+cout << "Enter your choice from 1,2,3,4" << endl;
+cin >> a;
+switch(a)
+{
+  case 1:
+  cout << "Current year is 2021" << endl;
+  break;
+  case 2:
+  cout << "Current month is October" << endl;
+  break;
+  case 3:
+  cout << "Day is wednesday" << endl;
+  break;
+  case 4:
+  cout << "invalid" << endl;
+  break;
+  default:
+   break;
+   }
+ return 0;
+   }
+   
+  
+  
