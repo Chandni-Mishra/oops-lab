5443d5aefc9442c1d1390e88959486f6cf6c9a87
Chandni-Mishra <91174476+Chandni-Mishra@users.noreply.github.com>
2021-10-21 22:13:13 +0530

    Create task5.cpp

---
+++ Practical-08/task5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int fact(int n);
+int main()
+{
+  int n;
+  cout<<"Enter the number you want factorial of"<<endl;
+  cin >> n;
+  cout << "Factorial of " << n << "=" << fact(n);
+  return 0;
+  }
+  int fact(int n)
+  {
+   if(n>1)
+   return n*fact(n-1);
+   else
+    return 1;
+    }
